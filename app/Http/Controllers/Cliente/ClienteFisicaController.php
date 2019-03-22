a69e76dad2fe269c578544e184aa244f3409c011
kelvin-Pereira <kelvinpereira.b18@gmail.com>
2019-03-22 14:16:01 -0300

    migrate funcionando

---
+++ app/Http/Controllers/Cliente/ClienteFisicaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Cliente;
+
+use App\Pessoa_Fisica;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ClienteFisicaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pessoa_Fisica  $pessoa_Fisica
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pessoa_Fisica $pessoa_Fisica)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pessoa_Fisica  $pessoa_Fisica
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pessoa_Fisica $pessoa_Fisica)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pessoa_Fisica  $pessoa_Fisica
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pessoa_Fisica $pessoa_Fisica)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pessoa_Fisica  $pessoa_Fisica
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pessoa_Fisica $pessoa_Fisica)
+    {
+        //
+    }
+}
---
+++ resources/views/Advogado/creat.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('conteudo')
+<br>
+<div class="main main-raised">
+    <h1 class="text-center">Cadastro de Advogado</h1><hr>
+    <div class="container">
+           <form action="" method="post">
+               @csrf
+            <div class="row">
+                <div class=" col-3">
+                     <div class="form-group bmd-form-group">
+                         <label class="bmd-label-floating"> Name Completo</label>
+                         <input type="text" class="form-control" name="nome" required>
+                     </div>
+                </div>
+                <div class=" col-2">
+                     <div class="form-group bmd-form-group">
+                         <label class="bmd-label-floating"> Registro OAB</label>
+                         <input type="text" class="form-control" name="OAB" required>
+                     </div>
+                </div>
+                <div class=" col-3">
+                     <div class="form-group bmd-form-group">
+                         <label class="bmd-label-floating"> E-mail</label>
+                         <input type="email" class="form-control" name="email" required>
+                     </div>
+                </div>
+                <div class=" col-2">
+                     <div class="form-group bmd-form-group">
+                         <label class="bmd-label-floating"> Telefone</label>
+                         <input type="text" class="form-control" name="telefone1" required>
+                     </div>
+                </div>
+                <div class=" col-2">
+                     <div class="form-group bmd-form-group">
+                         <label class="bmd-label-floating"> Telefone</label>
+                         <input type="text" class="form-control" name="telefone2">
+                     </div>
+                </div>
+                <div class=" col-2">
+                     <div class="form-group">
+                            <label for="exampleFormControlSelect1"></label>
+                            <select class="form-control selectpicker bmd-label-floating" data-style="btn btn-link" id="exampleFormControlSelect1" name="NivelAcesso" required>
+                                    <option value="">Perfil</option>
+                                    <option value="A">Admin  </option>
+                                    <option value="F">Funcionario </option>
+                            </select>
+                     </div>
+                </div>
+                <div class=" col-2">
+                     <div class="form-group">
+                            <label for="exampleFormControlSelect1"></label>
+                            <select class="form-control selectpicker bmd-label-floating" data-style="btn btn-link" id="exampleFormControlSelect1" name="AreaAtuacao" required>
+                                    <option value="">Area Atuação</option>
+                                    <option value="Familiar">Familiar</option>
+                                    <option value="Palhaço">Palhaço</option>
+                            </select>
+                     </div>
+                </div>
+            </div>  <!-- and row -->
+                <br>
+               <!-- Submit -->
+               <div class="text-right">
+                    <button type="reset" class="btn ">Limpar</button>
+                    <button type="submit" class="btn btn-success">Enviar</button>
+                </div>
+           </form>
+    </div>
+</div>
+@endsection
